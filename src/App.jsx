517e0669dfe990c8b2e241c87912112b60745295
Mark-Maher <markmaher383@gmailgit config --global user.email markmaher383@gmailgit config --global user.email markmaher383@gmail>
2024-02-04 17:20:50 +0200

    sc

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
           <Route path='/about' element={<About />} />
           <Route path='/portfolio' element={<Portfolio />} />
           <Route path='/contact' element={<Contact />} />
-          <Route path='firstReact/' element={<Home />} />
+          <Route path='/firstReact/' element={<Home />} />
         </Routes>
       </BrowserRouter>
       <Footer />
